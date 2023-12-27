a1c0e902a1e9827febad89d0757059dbfb82b813
Ayax0 <sg@vtt.ch>
2023-12-27 15:44:20 +0100

    init

---
+++ lua/plugins/lsp.lua
@@ -0,0 +1,34 @@
+return {
+  {
+    "williamboman/mason.nvim",
+    config = function()
+      require("mason").setup()
+    end
+  },
+  {
+    "williamboman/mason-lspconfig.nvim",
+    config = function()
+      require("mason-lspconfig").setup({
+        ensure_installed = {
+          "lua_ls",
+          "eslint",
+          "volar"
+        }
+      })
+    end
+  },
+  {
+    "neovim/nvim-lspconfig",
+    config = function()
+      local lspconfig = require("lspconfig")
+      -- LSP
+      lspconfig.lua_ls.setup({})
+      lspconfig.eslint.setup({})
+      lspconfig.volar.setup({})
+
+      vim.keymap.set('n', 'K', vim.lsp.buf.hover, {})
+      vim.keymap.set('n', 'gd', vim.lsp.buf.definition, {})
+      vim.keymap.set({ 'n', 'v' }, '<leader>ca', vim.lsp.buf.code_action, {})
+    end
+  }
+}
