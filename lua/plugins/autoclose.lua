cd8fafba1c1b5e9b8f2167c9b5ce682d12db01d7
Simon Gander <sg@vtt.ch>
2024-01-01 16:57:05 +0100

    add emmet support

--- lua/plugins/autoclose.lua
+++ lua/plugins/autoclose.lua
@@ -2,9 +2,14 @@ return {
   "m4xshen/autoclose.nvim",
   config = function()
     require("autoclose").setup({
+      --keys = {
+      --  ["<"] = { escape = false, close = true, pair = "<>" }
+      --},
       options = {
         disable_when_touch = true,
         touch_regex = "[%w(%[{]",
+        pair_spaces = true,
+        auto_indent = true
       },
     })
   end,
