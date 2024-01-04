bc75c90acc4481b54d0b261eb8c940fec8da7b08
Simon Gander <sg@vtt.ch>
2024-01-04 11:31:42 +0100

    add terminal

---
+++ lua/plugins/toggleterm.lua
@@ -0,0 +1,11 @@
+return {
+  "akinsho/toggleterm.nvim",
+  version = "*",
+  config = function()
+    require("toggleterm").setup({
+      start_in_insert = false,
+    });
+
+    vim.keymap.set('n', '<C-t>', ':ToggleTerm direction=float name=terminal<CR>', {})
+  end
+}
