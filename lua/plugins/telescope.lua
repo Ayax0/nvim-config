e0ef1d2b8d653e4e145a334ef94cf47056f09b8a
Simon Gander <sg@vtt.ch>
2024-01-05 17:26:26 +0100

    remove git keybinding

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -21,7 +21,7 @@ return {
 
     local builtin = require("telescope.builtin");
     vim.keymap.set("n", "<C-p>", builtin.find_files, {})
-    vim.keymap.set("n", "<C-g>", telescope.extensions.lazygit.lazygit, {})
+    -- vim.keymap.set("n", "<C-g>", telescope.extensions.lazygit.lazygit, {})
     vim.keymap.set("n", "<leader>fg", builtin.live_grep, {})
 
     vim.api.nvim_create_autocmd(
