61a3265417d1a34af15fdae17d1e4ec56975d916
Simon Gander <sg@vtt.ch>
2024-01-05 16:20:57 +0100

    add treesitter context

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -3,6 +3,12 @@ return {
   build = ":TSUpdate",
   dependencies = {
     { "windwp/nvim-ts-autotag" },
+    {
+      "nvim-treesitter/nvim-treesitter-context",
+      config = function()
+        require("treesitter-context").setup()
+      end
+    }
   },
   config = function()
     local config = require("nvim-treesitter.configs")
