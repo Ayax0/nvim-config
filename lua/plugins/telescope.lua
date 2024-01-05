3a90fd202c27a9297ab61fcdf6e663c82eff491e
Simon Gander <sg@vtt.ch>
2024-01-05 17:14:28 +0100

    add lazy git

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -3,10 +3,12 @@ return {
   tag = "0.1.5",
   dependencies = {
     "nvim-lua/plenary.nvim",
+    "kdheepak/lazygit.nvim",
     "nvim-telescope/telescope-ui-select.nvim",
   },
   config = function()
     local telescope = require("telescope");
+    telescope.load_extension("lazygit");
     telescope.load_extension("ui-select");
 
     telescope.setup({
@@ -19,6 +21,16 @@ return {
 
     local builtin = require("telescope.builtin");
     vim.keymap.set("n", "<C-p>", builtin.find_files, {})
+    vim.keymap.set("n", "<C-g>", telescope.extensions.lazygit.lazygit, {})
     vim.keymap.set("n", "<leader>fg", builtin.live_grep, {})
+
+    vim.api.nvim_create_autocmd(
+      "BufEnter",
+      {
+        callback = function()
+          require('lazygit.utils').project_root_dir()
+        end
+      }
+    )
   end,
 }
