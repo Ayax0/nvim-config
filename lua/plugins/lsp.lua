4189c89d091b73d7578051c3951a797cf210cfd9
Simon Gander <sg@vtt.ch>
2024-01-14 14:17:10 +0100

    add lsp keymaps

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -43,51 +43,9 @@ return {
     -- })
 
     lsp.setup();
+
+    vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
+    vim.keymap.set("n", "gd", vim.lsp.buf.definition, {})
+    vim.keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, {})
   end
 }
--- return {
---   {
---     "williamboman/mason.nvim",
---     config = function()
---       require("mason").setup()
---     end,
---   },
---   {
---     "williamboman/mason-lspconfig.nvim",
---     config = function()
---       require("mason-lspconfig").setup({
---         ensure_installed = {
---           "lua_ls",
---           "volar",
---           "cssmodules_ls",
---           "emmet_ls",
---         },
---       })
---     end,
---   },
---   {
---     "neovim/nvim-lspconfig",
---     config = function()
---       local capabilities = require("cmp_nvim_lsp").default_capabilities()
---
---       local lspconfig = require("lspconfig")
---       -- LSP
---       lspconfig.lua_ls.setup({
---         capabilities = capabilities,
---       })
---       lspconfig.volar.setup({
---         capabilities = capabilities,
---         filetypes = { 'typescript', 'javascript', 'javascriptreact', 'typescriptreact', 'vue', 'json' }
---       })
---       lspconfig.cssmodules_ls.setup({
---         capabilities = capabilities,
---       })
---       lspconfig.emmet_ls.setup({
---         capabilities = capabilities
---       })
---       vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
---       vim.keymap.set("n", "gd", vim.lsp.buf.definition, {})
---       vim.keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, {})
---     end,
---   },
--- }
