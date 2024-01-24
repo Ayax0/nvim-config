f91529e122fd61da247a1cb7e37efafde122bd18
Simon Gander <simon.gander@ioz.ch>
2024-01-24 08:16:08 +0100

    switch to nvim tree

---
+++ lua/plugins/nvim-tree.lua
@@ -0,0 +1,21 @@
+return {
+	"nvim-tree/nvim-tree.lua",
+	dependencies = {
+		"nvim-tree/nvim-web-devicons",
+	},
+	config = function()
+		require("nvim-tree").setup({
+			view = {
+				width = 40,
+			},
+			renderer = {
+				group_empty = true,
+			},
+			filters = {
+				dotfiles = true,
+			},
+		})
+
+		vim.keymap.set("n", "<C-n>", ":NvimTreeToggle<CR>", {})
+	end,
+}
