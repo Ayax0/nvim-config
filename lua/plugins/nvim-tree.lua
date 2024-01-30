c955ff903dc2b03128a37f4f6b6008b55d3b0a09
Simon Gander <simon.gander@ioz.ch>
2024-01-30 13:21:46 +0100

    remap term and explorer to leader

--- lua/plugins/nvim-tree.lua
+++ lua/plugins/nvim-tree.lua
@@ -1,21 +1,21 @@
 return {
-	"nvim-tree/nvim-tree.lua",
-	dependencies = {
-		"nvim-tree/nvim-web-devicons",
-	},
-	config = function()
-		require("nvim-tree").setup({
-			view = {
-				width = 40,
-			},
-			renderer = {
-				group_empty = true,
-			},
-			filters = {
-				dotfiles = true,
-			},
-		})
+  "nvim-tree/nvim-tree.lua",
+  dependencies = {
+    "nvim-tree/nvim-web-devicons",
+  },
+  config = function()
+    require("nvim-tree").setup({
+      view = {
+        width = 40,
+      },
+      renderer = {
+        group_empty = true,
+      },
+      filters = {
+        dotfiles = true,
+      },
+    })
 
-		vim.keymap.set("n", "<C-n>", ":NvimTreeToggle<CR>", {})
-	end,
+    vim.keymap.set("n", "<leader>n", ":NvimTreeToggle<CR>", {})
+  end,
 }
--- lua/plugins/toggleterm.lua
+++ lua/plugins/toggleterm.lua
@@ -5,9 +5,10 @@ return {
     require("toggleterm").setup({
       start_in_insert = false,
       auto_scroll = true,
-    });
+    })
 
-    vim.keymap.set('n', '<C-t>', ':ToggleTerm direction=float name=terminal<CR>', {});
-    vim.keymap.set('t', '<esc>', [[<C-\><C-n>]], {});
-  end
+    vim.keymap.set("n", "<leader>t", ":ToggleTerm direction=float name=terminal<CR>", {})
+    vim.keymap.set("t", "<esc>", [[<C-\><C-n>]], {})
+    -- vim.keymap.set("t", "<leader>t", [[<C-\><C-n>]], {})
+  end,
 }
