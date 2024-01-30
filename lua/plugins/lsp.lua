97229b0a34f72e65a5bf0ae8665623dbe77b5933
Simon Gander <sg@vtt.ch>
2024-01-30 09:23:40 +0100

    add lsp restart keymap

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -47,5 +47,6 @@ return {
     vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
     vim.keymap.set("n", "gd", vim.lsp.buf.definition, {})
     vim.keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, {})
+    vim.keymap.set("n", "<leader>rl", ":LspRestart<CR>", {})
   end
 }
