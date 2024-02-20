273954799013bcd02cbe14ed3d98d418eadfcf93
Simon Gander <simon.gander@ioz.ch>
2024-02-20 11:34:08 +0100

    dirty fix lsp crash

--- lua/vim-options.lua
+++ lua/vim-options.lua
@@ -6,3 +6,9 @@ vim.cmd("set number")
 vim.cmd("set splitbelow")
 vim.cmd("set relativenumber")
 vim.g.mapleader = " "
+
+vim.api.nvim_create_autocmd("BufWritePre", {
+	callback = function()
+		vim.cmd("LspRestart")
+	end,
+})
