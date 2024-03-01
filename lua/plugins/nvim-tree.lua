37e6ac17431311c80fb81bb631a90c9b5e9c2da8
Simon Gander <simon.gander@ioz.ch>
2024-03-01 14:26:22 +0100

    remove v mapping

--- lua/plugins/nvim-tree.lua
+++ lua/plugins/nvim-tree.lua
@@ -1,30 +1,29 @@
 return {
-  "nvim-tree/nvim-tree.lua",
-  dependencies = {
-    "nvim-tree/nvim-web-devicons",
-  },
-  config = function()
-    local api = require("nvim-tree.api")
+	"nvim-tree/nvim-tree.lua",
+	dependencies = {
+		"nvim-tree/nvim-web-devicons",
+	},
+	config = function()
+		local api = require("nvim-tree.api")
 
-    require("nvim-tree").setup({
-      view = {
-        width = 40,
-      },
-      renderer = {
-        group_empty = true,
-      },
-      filters = {
-        dotfiles = true,
-      },
-      on_attach = function(bufnr)
-        api.config.mappings.default_on_attach(bufnr)
+		require("nvim-tree").setup({
+			view = {
+				width = 40,
+			},
+			renderer = {
+				group_empty = true,
+			},
+			filters = {
+				dotfiles = true,
+			},
+			on_attach = function(bufnr)
+				api.config.mappings.default_on_attach(bufnr)
 
-        vim.keymap.set("n", "<C-t>", "", { buffer = bufnr })
-        vim.keymap.del("n", "<C-t>", { buffer = bufnr })
-      end,
-    })
+				vim.keymap.set("n", "<C-t>", "", { buffer = bufnr })
+				vim.keymap.del("n", "<C-t>", { buffer = bufnr })
+			end,
+		})
 
-    vim.keymap.set("n", "<C-n>", ":NvimTreeToggle<CR>", {})
-    vim.keymap.set("n", "v", api.node.open.vertical, {})
-  end,
+		vim.keymap.set("n", "<C-n>", ":NvimTreeToggle<CR>", {})
+	end,
 }
