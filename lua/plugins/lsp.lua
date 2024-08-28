326d81ca9dae57092c234344782df6dff2ec4ad0
Simon Gander <simon.gander@ioz.ch>
2024-08-28 22:20:01 +0200

    change eslint to biome

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -1,38 +1,48 @@
 return {
-  "VonHeikemen/lsp-zero.nvim",
-  branch = "v2.x",
-  dependencies = {
-    { "neovim/nvim-lspconfig" },
-    { "williamboman/mason.nvim" },
-    { "williamboman/mason-lspconfig.nvim" },
-  },
-  config = function()
-    local lsp = require("lsp-zero")
-    lsp.preset("minimal")
+	"VonHeikemen/lsp-zero.nvim",
+	branch = "v2.x",
+	dependencies = {
+		{ "neovim/nvim-lspconfig" },
+		{ "williamboman/mason.nvim" },
+		{ "williamboman/mason-lspconfig.nvim" },
+	},
+	config = function()
+		local lsp = require("lsp-zero")
+		lsp.preset("minimal")
 
-    lsp.set_sign_icons({
-      error = "✘",
-      warn = "▲",
-      hint = "⚑",
-      info = "»",
-    })
+		lsp.set_sign_icons({
+			error = "✘",
+			warn = "▲",
+			hint = "⚑",
+			info = "»",
+		})
 
-    local capabilities = vim.lsp.protocol.make_client_capabilities()
-    capabilities.textDocument.foldingRange = {
-      dynamicRegistration = false,
-      lineFoldingOnly = true,
-    }
+		local capabilities = vim.lsp.protocol.make_client_capabilities()
+		capabilities.textDocument.foldingRange = {
+			dynamicRegistration = false,
+			lineFoldingOnly = true,
+		}
 
-    local lspconfig = require("lspconfig")
-    lspconfig.volar.setup({
-      filetypes = { "typescript", "javascript", "javascriptreact", "typescriptreact", "vue", "json" },
-    })
+		local lspconfig = require("lspconfig")
 
-    lsp.setup()
+		lspconfig.volar.setup({
+			filetypes = {
+				"typescript",
+				"javascript",
+				"javascriptreact",
+				"typescriptreact",
+				"vue",
+				"json",
+			},
+		})
 
-    vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
-    vim.keymap.set("n", "gd", vim.lsp.buf.definition, {})
-    vim.keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, {})
-    vim.keymap.set("n", "<leader>rl", ":LspRestart<CR>", {})
-  end,
+		lspconfig.biome.setup({})
+
+		lsp.setup()
+
+		vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
+		vim.keymap.set("n", "gd", vim.lsp.buf.definition, {})
+		vim.keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, {})
+		vim.keymap.set("n", "<leader>rl", ":LspRestart<CR>", {})
+	end,
 }
--- lua/plugins/none-ls.lua
+++ lua/plugins/none-ls.lua
@@ -1,38 +1,38 @@
 return {
-  "nvimtools/none-ls.nvim",
-  dependencies = {
-    { "jay-babu/mason-null-ls.nvim" }
-  },
-  config = function()
-    local none_ls_mason = require("mason-null-ls");
-    local none_ls = require("null-ls");
+	"nvimtools/none-ls.nvim",
+	dependencies = {
+		{ "jay-babu/mason-null-ls.nvim" },
+	},
+	config = function()
+		local none_ls_mason = require("mason-null-ls")
+		local none_ls = require("null-ls")
 
-    none_ls_mason.setup({
-      ensure_installed = {
-        "stylua",
-        "eslint_d",
-      }
-    })
+		none_ls_mason.setup({
+			ensure_installed = {
+				"stylua",
+				"biome",
+			},
+		})
 
-    none_ls.setup({
-      sources = {
-        none_ls.builtins.formatting.stylua,
+		none_ls.setup({
+			sources = {
+				none_ls.builtins.formatting.stylua,
 
-        none_ls.builtins.formatting.eslint_d,
-        none_ls.builtins.diagnostics.eslint_d,
-      },
-      on_attach = function(client, bufnr)
-        if client.supports_method("textDocument/formatting") then
-          vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
-          vim.api.nvim_create_autocmd("BufWritePre", {
-            group = augroup,
-            buffer = bufnr,
-            callback = function()
-              vim.lsp.buf.format({ bufnr = bufnr })
-            end,
-          })
-        end
-      end,
-    })
-  end,
+				none_ls.builtins.formatting.biome,
+				none_ls.builtins.diagnostics.biome,
+			},
+			on_attach = function(client, bufnr)
+				if client.supports_method("textDocument/formatting") then
+					vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
+					vim.api.nvim_create_autocmd("BufWritePre", {
+						group = augroup,
+						buffer = bufnr,
+						callback = function()
+							vim.lsp.buf.format({ bufnr = bufnr })
+						end,
+					})
+				end
+			end,
+		})
+	end,
 }
